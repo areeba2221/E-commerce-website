76804d82fb09d5228b73a416068e4bd23eb438b9
areebafatima2205 <areebafatima2205@gmail.com>
2026-06-02 20:09:06 +0500

    link change

--- src/components/features/ShopFilterBar.jsx
+++ src/components/features/ShopFilterBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { shopFilterData } from "/src/Data/data";
+import { shopFilterData } from "/src/data/Data";
 import { FilterRing, Grid, Viewlist } from "/src/assets/Svg";
 
 const ShopFilterBar = () => {
